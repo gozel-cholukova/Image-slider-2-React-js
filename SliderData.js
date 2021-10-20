04be0d0c3f009eb842e8b9a260e6fe3a3cb04318
Gozel Cholukova <checkboxtm@gmail.com>
2021-10-21 01:43:34 +0500

    Create SliderData.js

---
+++ SliderData.js
@@ -0,0 +1,21 @@
+import gallery3 from "./lobby/gallery3.jpg";
+import gallery4 from "./lobby/gallery4.jpg";
+import gallery5 from "./lobby/gallery5.jpg";
+import gallery6 from "./lobby/gallery6.jpg";
+import gallery7 from "./lobby/gallery7.jpg";
+
+export const images = [
+  { img: gallery3 },
+  {   
+    img: gallery4
+  },
+  {
+    img: gallery5
+  },
+  {
+    img: gallery6
+  },
+  {
+    img: gallery7
+  },
+];
